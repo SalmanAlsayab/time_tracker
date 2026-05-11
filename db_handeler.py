cbead3d3969dae74855d0c6886f7dac80709e2ef
salman alsayab <alzlmsiyab@outlook.com>
2026-05-11 05:21:20 +0300

    small development of the project

---
+++ db_handeler.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+con = sqlite3.connect("applications_time.db")
+cur = con.cursor()
+
+
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from time-tracker!")
+from pydantic import BaseModel
+from win32gui import GetForegroundWindow, GetWindowText
+import re
+import time
+from datetime import datetime
+class application(BaseModel):
+    def __init__(self, name:str):
+        self.name = name
+        self.timer_step:int = 2
+        self.timer:int = 0
+        
+        def increase_timer():
+            self.timer += self.timer_step
+            
+time.sleep(2)
+# Get the handle to the foreground window
+hwnd = GetForegroundWindow()
+
+# Get the text (title) of that window
+window_title = GetWindowText(hwnd)
+print(f"Active Window: {window_title.split('-')}")
 
 
-if __name__ == "__main__":
-    main()
+            
