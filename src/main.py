0bae65da1ab328a3ecec6a55e3ac8d523eaa1431
salman alsayab <alzlmsiyab@outlook.com>
2026-07-16 05:07:21 +0300

    added unit tests for main function and exe path function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-import math
+from typing import ClassVar
 from datetime import datetime
 from win32gui import GetForegroundWindow, GetWindowText
 from uuid import uuid4
@@ -20,13 +20,24 @@ class ApplicationSession(BaseModel):
     # Define all fields here with type hints
     id: str = Field(default_factory=lambda: uuid4().hex)
     handle: int
-    timer_step: int = 2
     duration: int = 0
     start_datetime: datetime = Field(default_factory=datetime.now)
     inserted: bool = False
+    # Class variables
+    timer_step: ClassVar[int] = 2
 
     def increase_duration(self):
         self.duration += self.timer_step
+
+
+try:
+    base_dir = Path(__file__).resolve().parent.parent
+    icons_dir = base_dir / "icons"
+    icons_dir.mkdir(exist_ok=True)
+except FileNotFoundError:
+    raise FileNotFoundError("encountered issues in icons directory")
+except Exception:
+    raise Exception("encountered an unexpected error")
 
 
 def find_exe_path(handle: int):
@@ -72,14 +83,7 @@ def prepear_insert(session: ApplicationSession) -> dict:
     db_input["id"] = session.id
     exe_path = find_exe_path(handle=session.handle)
     db_input["name"] = exe_path.split("\\")[-1]
-    try:
-        base_dir = Path(__file__).resolve().parent.parent
-        icons_dir = base_dir / "icons"
-        icons_dir.mkdir(exist_ok=True)
-    except FileNotFoundError:
-        raise FileNotFoundError("encountered issues in icons directory")
-    except Exception:
-        raise Exception("encountered an unexpected error")
+
     for icon in icons_dir.iterdir():
         if f"{db_input['name']}.bmp" == icon:
             save_icon = False
@@ -119,37 +123,42 @@ def main():
     # check_duplicates_timer: int = 0
     create_table()
     while True:
-        sleep(current_session.timer_step)
-        # 1 strike = 2 seconds of not using app saved in current session
-        if strikes == 10:
-            current_session = next_session
-            next_session = ApplicationSession(handle=0)
-            strikes = 0
-        # make sure current session data is inserted and only once
-        if not current_session.inserted:
-            current_session.increase_duration()
-            insert_data = prepear_insert(current_session)
-            insert_into_table(insert_data)
-            current_session.inserted = True
-
-        if current_session.handle != hwnd:
-            current_next_handle = next_session.handle
-            if current_next_handle == 0:
-                next_session.handle = hwnd
-                next_session.increase_duration()
-                strikes += 1
-            elif current_next_handle == hwnd:
-                next_session.increase_duration()
-                strikes += 1
-            else:
+        try:
+            sleep(current_session.timer_step)
+            hwnd = GetForegroundWindow()
+            # 1 strike = 2 seconds of not using app saved in current session
+            if strikes == 10:
                 current_session = next_session
-                next_session = ApplicationSession(handle=hwnd)
-        else:
-            current_session.increase_duration()
-            update_data = prepear_update(current_session)
-            update_values(update_data)
-            strikes = 0
-        hwnd = GetForegroundWindow()
+                next_session = ApplicationSession(handle=0)
+                strikes = 0
+            # make sure current session data is inserted and only once
+            if not current_session.inserted:
+                current_session.increase_duration()
+                insert_data = prepear_insert(current_session)
+                insert_into_table(insert_data)
+                current_session.inserted = True
+
+            if current_session.handle != hwnd:
+                current_next_handle = next_session.handle
+                next_session.increase_duration()
+                if current_next_handle == 0:
+                    next_session.handle = hwnd
+                    strikes += 1
+                elif current_next_handle == hwnd:
+                    strikes += 1
+                else:
+                    current_session = next_session.model_copy()
+                    next_session = ApplicationSession(handle=hwnd)
+                    strikes = 0
+            else:
+                current_session.increase_duration()
+                update_data = prepear_update(current_session)
+                update_values(update_data)
+                strikes = 0
+        except psutil.NoSuchProcess as e:
+            print(e)
+            current_session = ApplicationSession(handle=hwnd)
+            next_session = ApplicationSession(handle=0)
 
 
 if __name__ == "__main__":
